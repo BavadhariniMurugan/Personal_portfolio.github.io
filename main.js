82d62e0bb0c6762d23caabd11697fc0bacd5c007
BavadhariniMurugan <141812137+BavadhariniMurugan@users.noreply.github.com>
2024-07-15 15:56:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "UX UI Designer", " Full Stack Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
